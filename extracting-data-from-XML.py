7f695829c52e04ae354363d6f1215e6bff990a45
Akash Gupta <80875509+imakashgupta@users.noreply.github.com>
2021-07-06 13:48:46 +0530

    Add files via upload

---
+++ extracting-data-from-XML.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+import urllib.request, urllib.parse, urllib.error
+import ssl
+total = 0
+data = input('Enter the link:')
+url = urllib.request.urlopen(data).read()
+
+stuff = ET.fromstring(url)
+lst = stuff.findall('comments/comment')
+print('List Length:', len(lst))
+for item in lst:
+    Num = item.find('count').text
+    iNum = int(Num)
+    total = total + iNum
+    print(total)
+print('Total number of comments:', total)
+
